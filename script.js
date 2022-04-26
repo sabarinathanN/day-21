a7ca893960b45196566a7082303f738f5ff4e956
Sabarinathan N <98639811+sabarinathanN@users.noreply.github.com>
2022-04-26 12:34:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+setTimeout(()=>{
+    document.write("1");
+    document.write('<br/>');
+},0000)
+
+setTimeout(()=>{
+    document.write("2");
+    document.write('<br/>');
+},1000)
+//
+
+
+setTimeout(()=>{
+    document.write("3");
+    document.write('<br/>');
+},2000)
+//
+
+setTimeout(()=>{
+    document.write("4");
+    document.write('<br/>');
+},3000)
+//
+
+setTimeout(()=>{
+    document.write("5");
+    document.write('<br/>');
+},4000)
+//
+
+setTimeout(()=>{
+    document.write("6");
+    document.write('<br/>');
+},5000)
+//
+
+setTimeout(()=>{
+    document.write("7");
+    document.write('<br/>');
+},6000)
+
+//
+
+setTimeout(()=>{
+    document.write("8");
+    document.write('<br/>');
+},7000)
+//
+
+setTimeout(()=>{
+    document.write("9");
+    document.write('<br/>');
+},8000)
+
+//
+
+setTimeout(()=>{
+    document.write("10");
+    document.write('<br/>');
+},9000)
+var br1=document.createElement("br");
+setTimeout(()=>{
+    document.write('<br/>');
+    document.write("HAPPY INDEPENDENCE DAY");
+   
+},10000)
